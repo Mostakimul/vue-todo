81d92a1da43eebcb9cd998d584c62c663a7cd3c9
Mostakimul Karim <mostakimulkarim@gmail.com>
2023-02-06 01:29:14 -0500

    feat: adding todo and error msg

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,12 +1,43 @@
+<script setup>
+import { defineEmits, reactive } from 'vue';
+// reactive using ref
+const todoState = reactive({
+  todo: '',
+  invalid: null,
+  errMsg: '',
+});
+
+const emit = defineEmits(['create-todo']);
+
+const createTodo = () => {
+  todoState.invalid = null;
+  if (todoState.todo !== '') {
+    emit('create-todo', todoState.todo);
+    todoState.todo = '';
+    return;
+  }
+  todoState.invalid = true;
+  todoState.errMsg = 'You must enter todo value';
+};
+</script>
+
 <template>
-  <div class="text-center mt-5">
-    <input type="text" class="rounded-md p-1 shadow-md" />
+  <div class="flex justify-center items-start py-5">
+    <div>
+      <input
+        v-model="todoState.todo"
+        type="text"
+        class="rounded-md p-1 shadow-md"
+      />
+      <p v-show="todoState.invalid" class="text-red-500">
+        {{ todoState.errMsg }}
+      </p>
+    </div>
     <button
+      @click="createTodo"
       class="px-5 bg-green-500 rounded-md mx-5 py-1 ring-1 ring-gray-800 font-semibold shadow-md"
     >
       Create
     </button>
   </div>
 </template>
-
-<script setup></script>
--- src/views/TodosView.vue
+++ src/views/TodosView.vue
@@ -1,10 +1,22 @@
 <script setup>
+import { uid } from 'uid';
+import { ref } from 'vue';
 import TodoInput from '../components/TodoInput.vue';
+
+const todos = ref([]);
+const createTodo = (todo) => {
+  todos.value.push({
+    id: uid(),
+    todo,
+    isCompleted: null,
+    isEditing: null,
+  });
+};
 </script>
 
 <template>
   <section class="bg-slate-300 rounded-md mt-5 p-5">
     <h1 class="font-semibold text-center text-xl">Create Todos</h1>
-    <TodoInput />
+    <TodoInput @create-todo="createTodo" />
   </section>
 </template>
